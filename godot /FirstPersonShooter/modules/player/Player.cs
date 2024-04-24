4bc08f029c262a26f87b428baead21bc41c47400
Yunier Alvarez <yunieralvarez@gmail.com>
2024-04-23 20:21:26 -0400

    format

--- godot /FirstPersonShooter/modules/player/Player.cs
+++ godot /FirstPersonShooter/modules/player/Player.cs
@@ -1,38 +1,41 @@
 using Godot;
 
-public partial class Player : CharacterBody3D {
+public partial class Player : CharacterBody3D
+{
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta) {
+	public override void _PhysicsProcess(double delta)
+	{
 		Vector3 velocity = this.Velocity;
 
 		// Add the gravity.
-		if (!this.IsOnFloor()) {
+		if (!this.IsOnFloor())
+		{
 			velocity.Y -= this.gravity * (float)delta;
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && this.IsOnFloor()) {
+		if (Input.IsActionJustPressed("ui_accept") && this.IsOnFloor())
+		{
 			velocity.Y = JumpVelocity;
 		}
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		var inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
 		Vector3 direction = (
 			this.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)
 		).Normalized();
 
-		if (direction != Vector3.Zero) {
+		if (direction != Vector3.Zero)
+		{
 			velocity.X = direction.X * Speed;
 			velocity.Z = direction.Z * Speed;
 		}
-		else {
+		else
+		{
 			velocity.X = Mathf.MoveToward(this.Velocity.X, 0, Player.Speed);
 			velocity.Z = Mathf.MoveToward(this.Velocity.Z, 0, Player.Speed);
 		}
@@ -40,4 +43,14 @@ public partial class Player : CharacterBody3D {
 		this.Velocity = velocity;
 		_ = this.MoveAndSlide();
 	}
+
+	// private float ApplyGravity(float delta, Vector3 velocity)
+	// {
+	// 	if (!this.IsOnFloor())
+	// 	{
+	// 		velocity.Y -= this.gravity * (float)delta;
+	// 	}
+
+	// 	return 0.0
+	// }
 }
