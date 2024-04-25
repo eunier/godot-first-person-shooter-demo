e2165a6edf9ec1e93aa34981d5f22545cb7a5ee0
Yunier Alvarez <yunieralvarez@gmail.com>
2024-04-24 22:37:55 -0400

    equip gun

--- godot /FirstPersonShooter/modules/weapons/WeaponSystem.cs
+++ godot /FirstPersonShooter/modules/weapons/WeaponSystem.cs
@@ -1,10 +1,83 @@
+using System;
+using System.Linq;
 using Godot;
+
+public enum WeaponType
+{
+	Rifle,
+	Cannon,
+}
 
 public partial class WeaponSystem : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() { }
+	private readonly Godot.Collections.Dictionary<WeaponType, Node3D> weapons =
+		new();
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	private System.Collections.Generic.KeyValuePair<
+		WeaponType,
+		Node3D
+	> currentWeapon;
+
+	public override void _Ready()
+	{
+		this.GetChildrenWeapons();
+		this.Equip(WeaponType.Rifle);
+	}
+
 	public override void _Process(double delta) { }
+
+	private void GetChildrenWeapons()
+	{
+		var children = this.GetChildren();
+
+		var allWeaponsTypes = new WeaponType[]
+		{
+			WeaponType.Rifle,
+			WeaponType.Cannon,
+		};
+
+		if (children.Count != allWeaponsTypes.Length)
+		{
+			var msg = "Missing weapon(s).";
+			GD.PrintErr(msg);
+			throw new Exception(msg);
+		}
+
+		foreach (var child in children)
+		{
+			if (child is not Node3D)
+			{
+				var msg = "child is not a Node3D.";
+				GD.PrintErr(msg);
+				throw new Exception(msg);
+			}
+		}
+
+		var zip = children.Zip(allWeaponsTypes);
+
+		foreach (var (child, weaponType) in zip)
+		{
+			this.weapons.Add(weaponType, (Node3D)child);
+		}
+	}
+
+	private void Equip(WeaponType weaponType)
+	{
+		foreach (var kvp in this.weapons)
+		{
+			var (k, v) = kvp;
+
+			if (k == weaponType)
+			{
+				this.currentWeapon = kvp;
+				v.Visible = true;
+				v.SetProcess(true);
+			}
+			else
+			{
+				v.Visible = false;
+				v.SetProcess(false);
+			}
+		}
+	}
 }
