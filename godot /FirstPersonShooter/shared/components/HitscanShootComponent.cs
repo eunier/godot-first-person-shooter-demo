cf39fde484d402289e05dac52194af49c30cc0ca
Yunier Alvarez <yunieralvarez@gmail.com>
2024-04-26 20:49:22 -0400

    wip

---
+++ godot /FirstPersonShooter/shared/components/HitscanShootComponent.cs
@@ -0,0 +1,26 @@
+namespace App.Shared.Components
+{
+	using System;
+	using Godot;
+
+	public partial class HitscanShootComponent : Node3D
+	{
+		[Export]
+		private NodePath rayCastNodePath;
+		private RayCast3D rayCast;
+
+		public override void _Ready()
+		{
+			this.rayCast = this.GetNode<RayCast3D>(this.rayCastNodePath);
+
+			if (this.rayCast == null)
+			{
+				var msg = "RayCast3D not found.";
+				Logger.Print(msg);
+				throw new Exception(msg);
+			}
+		}
+
+		public override void _Process(double delta) { }
+	}
+}
