d98fe2cc64c2768e95958b514b7e2e032055ec06
Yunier Alvarez <yunieralvarez@gmail.com>
2024-04-26 23:38:12 -0400

    clean up

--- godot /FirstPersonShooter/modules/weapons/weapons/cannon/Cannon.cs
+++ godot /FirstPersonShooter/modules/weapons/weapons/cannon/Cannon.cs
@@ -1,18 +1,4 @@
 namespace App.Modules.Weapons
 {
-	using App.Shared;
-
-	public partial class Cannon : Weapon
-	{
-		// Called when the node enters the scene tree for the first time.
-		public override void _Ready() { }
-
-		// Called every frame. 'delta' is the elapsed time since the previous frame.
-		public override void _Process(double delta) { }
-
-		public void Shoot()
-		{
-			Logger.Print();
-		}
-	}
+	public partial class Cannon : Weapon { }
 }
--- godot /FirstPersonShooter/modules/weapons/weapons/rifle/Rifle.cs
+++ godot /FirstPersonShooter/modules/weapons/weapons/rifle/Rifle.cs
@@ -1,22 +1,4 @@
 namespace App.Modules.Weapons
 {
-	using Godot;
-
-	public partial class Rifle : Weapon
-	{
-		private GpuParticles3D muzzleFlash;
-
-		public override void _Ready()
-		{
-			this.muzzleFlash = this.GetNode<GpuParticles3D>("MuzzleFlash");
-		}
-
-		// Called every frame. 'delta' is the elapsed time since the previous frame.
-		public override void _Process(double delta) { }
-
-		public void Shoot()
-		{
-			this.muzzleFlash.Restart();
-		}
-	}
+	public partial class Rifle : Weapon { }
 }
