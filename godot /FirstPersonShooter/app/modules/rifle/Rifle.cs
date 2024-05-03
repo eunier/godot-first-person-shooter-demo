3c72f99f8e45476da0cbfbda5c66501026cff288
Yunier Alvarez <yunieralvarez@gmail.com>
2024-05-02 21:58:32 -0400

    use resource

--- godot /FirstPersonShooter/app/modules/rifle/Rifle.cs
+++ godot /FirstPersonShooter/app/modules/rifle/Rifle.cs
@@ -9,12 +9,14 @@ namespace App.Modules.RifleModule
 
 	public partial class Rifle : Weapon
 	{
-		private const float FireRate = 14;
-		private const int DamageAmount = 15; // TODO: load the resource
 		private const string FireRateTimerNodePath = "%FireRateTimer";
 		private const string HitscanNodePath = "%Hitscan";
 		private const string MuzzleFlashNodePath = "%MuzzleFlash";
 		private const string SparkControllerNodePath = "%SparkController";
+
+		[Export]
+		private WeaponResource? resource;
+
 		private GpuParticles3D? muzzleFlash;
 		private Hitscan? hitscan;
 		private SparkController? sparkController;
@@ -29,7 +31,7 @@ namespace App.Modules.RifleModule
 			);
 
 			this.fireRateTimer = this.GetNode<Timer>(Rifle.FireRateTimerNodePath);
-			this.fireRateTimer!.WaitTime = 1 / Rifle.FireRate;
+			this.fireRateTimer!.WaitTime = this.resource!.FireRateWaitTime;
 
 			this.muzzleFlash = this.GetNode<GpuParticles3D>(
 				Rifle.MuzzleFlashNodePath
@@ -46,7 +48,7 @@ namespace App.Modules.RifleModule
 			Logger.Print($"Shooting");
 			this.muzzleFlash!.Restart();
 			this.fireRateTimer!.Start();
-			var res = this.hitscan!.Shoot(camera, 100); // TODO: use resource
+			var res = this.hitscan!.Shoot(camera, this.resource!.Range);
 
 			if (res is not null)
 			{
@@ -54,7 +56,7 @@ namespace App.Modules.RifleModule
 
 				if (res.Collider is IWithHealth collider)
 				{
-					collider.Health.Damage(Rifle.DamageAmount);
+					collider.Health.Damage(this.resource!.Damage);
 				}
 			}
 
--- godot /FirstPersonShooter/app/modules/weapon/WeaponResource.cs
+++ godot /FirstPersonShooter/app/modules/weapon/WeaponResource.cs
@@ -6,15 +6,28 @@ namespace App.Modules.WeaponModule
 	public partial class WeaponResource : Resource
 	{
 		public WeaponResource()
-			: this(0, 0) { }
+			: this(0, 0, 0) { }
 
-		public WeaponResource(int damage, int range)
+		public WeaponResource(int damage, int range, int fireRatePerMinute)
 		{
 			this.Damage = damage;
 			this.Range = range;
+			this.FireRatePerMinute = fireRatePerMinute;
 		}
 
+		[Export]
 		public int Damage { get; set; }
+
+		[Export]
 		public int Range { get; set; }
+
+		[Export]
+		public float FireRatePerMinute { get; set; }
+
+		public float FireRateWaitTime
+		{
+			get => 60 / this.FireRatePerMinute;
+			private set { }
+		}
 	}
 }
